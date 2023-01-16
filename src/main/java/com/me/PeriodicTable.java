5d2c9e2ebc84659588464285e050bf77b758fb76
42784XM <3485376023@qq.com>
2023-01-16 12:49:37 +0800

    Init Commit

---
+++ src/main/java/com/me/PeriodicTable.java
@@ -0,0 +1,11 @@
+package com.me;
+
+/**
+ * @author 秃头老狗
+ * @version 1.0
+ */
+public class PeriodicTable {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
