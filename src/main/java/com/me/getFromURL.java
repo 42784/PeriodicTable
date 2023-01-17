8c09989a40c435c5090cd6ed76a12a0fc014c69e
42784XM <3485376023@qq.com>
2023-01-17 15:41:34 +0800

    requestToGetYAML Commit

--- src/main/java/com/me/getFromURL.java
+++ src/main/java/com/me/getFromURL.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -15,26 +16,41 @@ import java.net.URL;
  * <a href="https://ptable.com/JSON/properties-90d5338.json">...</a>
  */
 public class getFromURL {
-    public static void main(String[] args) throws IOException {
-        URL url = new URL("file:///D:/Downloads/formula=Li");
-
-
-        InputStream inputStream = url.openStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        BufferedWriter bufferedWriter =
-                new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\Users\\Administrator\\Desktop\\test.json")));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            bufferedWriter.write(line);
-            bufferedWriter.newLine();
-            System.out.println("line = " + line);
+    public static void main(String[] args) {
+        try {
+            requestToGetYAML(new URL("https://ptable.com/JSON/compounds/formula=Li"));
+        } catch (IOException e) {
+            System.err.println("获取服务器资源错误(请检查网络连接)");
         }
+
+
+    }
+    /**
+     * 请求服务器获取json并且自动转为yaml
+     */
+    public static void requestToGetYAML(URL url) throws IOException{
+
+        InputStream inputStream = url.openStream();//开启流
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));//缓冲读取
+        BufferedWriter bufferedWriter =
+                new BufferedWriter(
+                        new OutputStreamWriter(
+                                new FileOutputStream(
+                                        "C:\\Users\\Administrator\\Desktop\\test.yaml")));//文件写入
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {//读取流
+            stringBuilder.append(line).append('\n');
+        }
+
+        if (stringBuilder.toString().isEmpty()){
+            throw new IOException("未知的Stream错误");
+        }
+        //写入文件
+        bufferedWriter.write(stringBuilder.toString());
         bufferedWriter.flush();
 
-        System.out.println("jsonConvertToYaml() = " + jsonConvertToYaml());
-
-
-
+        System.out.println("jsonConvertToYaml() = " + jsonConvertToYaml(stringBuilder.toString()));
 
     }
 
